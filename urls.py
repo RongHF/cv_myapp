03485c19596f3bdba53d4ef6ea05e080a97755e2
rong_desktop <rong.huangfu280@gmail.com>
2018-09-07 10:23:15 -0500

    under development

--- urls.py
+++ urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
+    url(r'^$', views.index, name="home"),
+    url(r'^test/$', views.home),
+
     # url(r'^Results/', views.index, name='index'),
 ]
 
--- views.py
+++ views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    return render(request, 'myapp/myapp_home_underdevelopment.html')
+
+def home(request):
     return render(request, 'myapp/myapp_home.html')
